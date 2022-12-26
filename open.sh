6c6d66da88fd017439e063c0b061a851fcb35a60
planessr <109939648+planessr@users.noreply.github.com>
2022-12-27 01:13:02 +0800

    Update open.sh

--- open.sh
+++ open.sh
@@ -10,4 +10,4 @@ systemctl stop firewalld.service 2>/dev/null
     iptables -F 2>/dev/null
     iptables -X 2>/dev/null
     netfilter-persistent save 2>/dev/null
-    echo -e "${green}放开防火墙端口成功！${plain}"
+    echo -e "${green}端口自动放行开启成功！SsPlane友情赞助！${plain}"
